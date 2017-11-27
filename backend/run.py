55fc8452395c0fc54b0e8a0c32ce1f2e598e4e67
exponentialbit1024 <shah255@purdue.edu>
2017-11-26 20:45:32 -0500

    run fix

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,4 @@
-import os
-
-from app import create_app
-
-# config_name = os.getenv('FLASK_CONFIG')
-config_name = "development"
-app = create_app(config_name)
+from app import app
 
 if __name__ == '__main__':
     app.run(debug=True)
