6fe78d15dba5f146ae1773650715ef3692c9ada1
Dhiman Swadia <dhimanswadiachelsea@gmail.com>
2017-11-28 15:52:29 -0500

    Update endpoint

--- frontend/src/utils/apiUtil.js
+++ frontend/src/utils/apiUtil.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export {getAns};
 
 function getAns(searchTerm) {
-  const url = `http://syllabot.herokuapp.com/textIn`;
+  const url = `https://syllabot.herokuapp.com/textIn`;
   return axios.post(url, {
     input: searchTerm
 }).then(response => response.data);
